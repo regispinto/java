300ded4d7a166e5d312718be81c37d1286c8784b
Regis Pinto <regispinto65@gmail.com>
2022-04-01 11:42:03 -0300

    Tratado a exceção do metodo2 dentro do metodo1

--- Exceções/src/Fluxo.java
+++ Exceções/src/Fluxo.java
@@ -7,7 +7,11 @@ public class Fluxo {
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        metodo2();
+        try{
+            metodo2();
+        } catch(ArithmeticException ex) {
+            System.out.println("ArithmeticException");
+        }
         System.out.println("Fim do metodo1");
     }
 
@@ -15,11 +19,7 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
-            try{
-                int a = i / 0;
-            } catch(ArithmeticException ex) {
-                System.out.println("ArithmeticException");
-            }
+            int a = 1/0;
         }
         System.out.println("Fim do metodo2");
     }
