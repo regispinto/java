604f047ad6677ade6849094244165b48738f9756
Regis Pinto <regispinto65@gmail.com>
2022-04-01 11:39:20 -0300

    Tratado uma exceção diretamente no método causador (metod2)

--- Exceções/src/Fluxo.java
+++ Exceções/src/Fluxo.java
@@ -15,6 +15,11 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            try{
+                int a = i / 0;
+            } catch(ArithmeticException ex) {
+                System.out.println("ArithmeticException");
+            }
         }
         System.out.println("Fim do metodo2");
     }
