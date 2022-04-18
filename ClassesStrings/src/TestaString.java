f73f0affb18278a0eff8b22b878f7809eef0041a
Regis Pinto <regispinto65@gmail.com>
2022-04-18 12:05:54 -0300

    Mais exemplos da classe String

--- ClassesStrings/src/TestaString.java
+++ ClassesStrings/src/TestaString.java
@@ -1,13 +1,19 @@
+import java.util.Locale;
+
 public class TestaString {
     public static void main(String[] args) {
 
         String nome = "Alura"; // objeto literal
+      //Estância intrisica do objeto String
+      //String outro = new String("Alura");
 
-        //String outro = new String("Alura");
-
-        String outro = nome.replace("A", "a");
+        String aux = nome.replace("A", "a");
+        String aux1 = nome.toLowerCase();
+        String aux2 = nome.toUpperCase();
 
         System.out.println(nome);
-        System.out.println(outro);
+        System.out.println(aux);
+        System.out.println(aux1);
+        System.out.println(aux2);
     }
 }
