67dcba3e05be7b43327019789962ed79dbbad1b3
Regis Pinto <regispinto65@gmail.com>
2022-04-18 11:51:00 -0300

    Entendo a classe String

---
+++ ClassesStrings/src/TestaString.java
@@ -0,0 +1,12 @@
+public class TestaString {
+    public static void main(String[] args) {
+        String nome = "Alura"; // objeto literal
+
+        //String outro = new String("Alura");
+
+        String outro = nome.replace("A", "a");
+
+        System.out.println(nome);
+        System.out.println(outro);
+    }
+}
