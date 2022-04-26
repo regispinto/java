bc5e6d309e967e3edaf5fc2776d2d1ff0eb4cac1
Regis Pinto <regispinto65@gmail.com>
2022-04-26 17:30:12 -0300

    Lições aprendidas:
    uma array do tipo Object pode guardar qualquer tipo de referência
    quando convertemos uma referência genérica para uma referência mais específica é preciso usar um type cast
    o cast só compila quando é possível, mesmo assim pode falhar na hora de rodar
    quando o type cast falha podemos receber uma ClassCastException
    para receber valores ao chamar o programa Java na linha de comando podemos usar o array String[] no método main

---
+++ OOP-Polimorfismo-Fixação/src/Testes/TestaArrayDeStrings.java
@@ -0,0 +1,11 @@
+package Testes;
+
+public class TestaArrayDeStrings {
+    public static void main(String[] args) {
+        System.out.println("Funcionou!!");
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
