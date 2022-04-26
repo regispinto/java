c2b65b271176f9ba44a777eaff3e15ec259c5b02
Regis Pinto <regispinto65@gmail.com>
2022-04-26 15:21:26 -0300

    Aprendendo sobre estrutura de dados - Abstraindo objetos e classes

--- OOP-Polimorfismo-Fixação/src/Testes/TestaArrayDeReferencias.java
+++ OOP-Polimorfismo-Fixação/src/Testes/TestaArrayDeReferencias.java
@@ -1,28 +1,27 @@
 package Testes;
 
+import Classes.Conta;
 import Classes.ContaCorrente;
+import Classes.ContaPoupanca;
 
 public class TestaArrayDeReferencias {
     public static void main(String[] args) {
-        // Array de Referências (Objeto Conta Corrente)
-        ContaCorrente[] contas = new ContaCorrente[5];
+        // Array de Referências (Objeto Conta Corrente/Poupança)
+        Conta[] contas = new Conta[5];
 
         // Objeto Conta Corrente
         ContaCorrente cc1 = new ContaCorrente(22, 11);
         contas[0] = cc1;
 
-        ContaCorrente cc2 = new ContaCorrente(22, 22);
+        ContaPoupanca cc2 = new ContaPoupanca(33, 44);
         contas[1] = cc2;
 
-        ContaCorrente referencia = contas[1];
+        ContaPoupanca referencia = (ContaPoupanca) contas[1]; // Type Cast
         System.out.println(referencia.getNumero());
 
-        System.out.println(cc2.getNumero());
-
-        System.out.println(cc2.getNumero());
-
-        // Chamando o getNumero() da classe ContaCorrente através do array contas[]
-        System.out.println(contas[1].getNumero());
+        // Chamando o método getNumero() da classe ContaPoupanca através do array contas[]
+        System.out.println("Numero: " + contas[1].getNumero());
+        System.out.println("Agência: " + contas[1].getAgencia());
 
         // Declaração de arrays de forma Literal, nesse contexto, significa usar valores diretamente,
         // menos burocrático, mais direito
