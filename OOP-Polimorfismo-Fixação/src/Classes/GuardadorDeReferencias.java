3c842c2cf846dd2780d80d3b76b8da9dfe14c1d6
Regis Pinto <regispinto65@gmail.com>
2022-04-27 17:05:03 -0300

    Teste da classe GuardadorDeReferencias de Arrays de referência para abstração do uso de arrays

---
+++ OOP-Polimorfismo-Fixação/src/Classes/GuardadorDeReferencias.java
@@ -0,0 +1,25 @@
+package Classes;
+
+public class GuardadorDeReferencias {
+
+    private Object[] referencias;
+    private int posicaoLivre;
+
+    public GuardadorDeReferencias() {
+        this.referencias = new Object[10];
+    }
+
+    public void adiciona(Object ref) {
+        this.referencias[this.posicaoLivre] = ref;
+        this.posicaoLivre++;
+    }
+
+    public int getQuantidadeDeElementos() {
+        return this.posicaoLivre;
+    }
+
+    public Object getReferencia(int posicao) {
+        return this.referencias[posicao];
+    }
+
+}
---
+++ OOP-Polimorfismo-Fixação/src/Testes/TestaGuardadorDeReferencias.java
@@ -0,0 +1,27 @@
+package Testes;
+
+import Classes.Conta;
+import Classes.ContaCorrente;
+import Classes.GuardadorDeContas;
+import Classes.GuardadorDeReferencias;
+
+public class TestaGuardadorDeReferencias {
+
+    public static void main(String[] args) {
+        GuardadorDeReferencias guardador = new GuardadorDeReferencias();
+
+        Conta cc1 = new ContaCorrente(22, 11);
+        guardador.adiciona(cc1);
+
+        Conta cc2 = new ContaCorrente(22, 522);
+        guardador.adiciona(cc2);
+
+        int tamanho = guardador.getQuantidadeDeElementos();
+
+        System.out.println(tamanho);
+
+        Conta ref = (Conta) guardador.getReferencia(1);
+        System.out.println(ref.getNumero());
+    }
+
+}
