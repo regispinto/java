900158e9b070bf9d01146534d36d8080cbc72fea
Regis Pinto <regispinto65@gmail.com>
2022-04-29 15:40:04 -0300

    Entendendo List<String> com o String[] de argumentos

--- OOP-Polimorfismo-Fixação/src/Testes/TestaArrayDeStrings.java
+++ OOP-Polimorfismo-Fixação/src/Testes/TestaArrayDeStrings.java
@@ -1,11 +1,23 @@
 package Testes;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class TestaArrayDeStrings {
     public static void main(String[] args) {
-        System.out.println("Funcionou!!");
 
+        // Lista o String[] com os argumentos do array de strings
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
+        }
+
+        // Atribui a uma lista os argumentos atribuidos em args
+        List<String> argumentos = Arrays.asList(args);
+
+        System.out.println("----- List<String> -----");
+
+        for (int i = 0; i < argumentos.size(); i++) {
+            System.out.println(argumentos.get(i));
         }
     }
 }
