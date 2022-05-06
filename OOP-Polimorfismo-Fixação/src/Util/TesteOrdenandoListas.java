2a412df37cac1db9b1585d6bbc5b343023e0919a
Regis Pinto <regispinto65@gmail.com>
2022-05-06 20:34:59 -0300

    Ordenando Listas Contas - Número/Titular

--- OOP-Polimorfismo-Fixação/src/Util/TesteOrdenandoListas.java
+++ OOP-Polimorfismo-Fixação/src/Util/TesteOrdenandoListas.java
@@ -1,5 +1,6 @@
 package Util;
 
+import Classes.Cliente;
 import Classes.Conta;
 import Classes.ContaCorrente;
 import Classes.ContaPoupanca;
@@ -10,6 +11,7 @@ import java.util.List;
 
 public class TesteOrdenandoListas {
     public static void main(String[] args) {
+        //
         Conta cc1 = new ContaCorrente(22, 33);
         cc1.deposita(333.0);
 
@@ -22,41 +24,88 @@ public class TesteOrdenandoListas {
         Conta cc4 = new ContaPoupanca(22, 22);
         cc4.deposita(222.0);
 
-        List<Conta> lista = new ArrayList<>();
-        lista.add(cc1);
-        lista.add(cc2);
-        lista.add(cc3);
-        lista.add(cc4);
+        List<Conta> lista1 = new ArrayList<>();
+        lista1.add(cc1);
+        lista1.add(cc2);
+        lista1.add(cc3);
+        lista1.add(cc4);
 
-        for (Conta conta: lista) {
+        System.out.println("-- [LISTA PELO NÚMERO DA CONTA] ---------------------------------");
+        for (Conta conta: lista1) {
             System.out.println(conta);
         }
+        OrdenaPeloNumeroDaConta comparator = new OrdenaPeloNumeroDaConta();
+        lista1.sort(comparator);
 
-        OrdenaListaDeContas comparator = new OrdenaListaDeContas();
-        lista.sort(comparator);
+        System.out.println("-- Lista Ordenada pelo número da conta ---------------------------");
 
-        System.out.println("-- Lista Ordenada ---------------------------");
-
-        for (Conta conta: lista) {
+        for (Conta conta: lista1) {
             System.out.println(conta);
+        }
+        //
+        System.out.println("-- [LISTA PELO TITULAR DA CONTA] ---------------------------------");
+        Conta cc5 = new ContaCorrente(22, 33);
+        Cliente clienteCC5 = new Cliente();
+        clienteCC5.setNome("Nico");
+        cc5.setTitular(clienteCC5);
+        cc5.deposita(333.0);
+
+        Conta cc6 = new ContaPoupanca(22, 44);
+        Cliente clienteCC6 = new Cliente();
+        clienteCC6.setNome("Neco");
+        cc6.setTitular(clienteCC6);
+        cc6.deposita(444.0);
+
+        Conta cc7 = new ContaCorrente(22, 11);
+        Cliente clienteCC7 = new Cliente();
+        clienteCC7.setNome("Paulo");
+        cc7.setTitular(clienteCC7);
+        cc7.deposita(111.0);
+
+        Conta cc8 = new ContaPoupanca(22, 22);
+        Cliente clienteCC8 = new Cliente();
+        clienteCC8.setNome("Ana");
+        cc8.setTitular(clienteCC8);
+        cc8.deposita(222.0);
+
+        List<Conta> lista2 = new ArrayList<>();
+        lista2.add(cc5);
+        lista2.add(cc6);
+        lista2.add(cc7);
+        lista2.add(cc8);
+        //
+        for (Conta conta: lista2) {
+            System.out.println(conta);
+        }
+        OrdenaPeloNomeDoTitular nomeComparator = new OrdenaPeloNomeDoTitular();
+        lista2.sort(nomeComparator);
+
+        System.out.println("-- Lista Ordenada pelo Nome do Titular ---------------------------");
+
+        for (Conta conta: lista2) {
+            System.out.println(conta + ", " + conta.getTitular().getNome());
         }
     }
 }
 
-class OrdenaListaDeContas implements Comparator<Conta> {
-
+class OrdenaPeloNumeroDaConta implements Comparator<Conta> {
     @Override
     public int compare(Conta c1, Conta c2) {
         if(c1.getNumero() < c2.getNumero()) {
             return -1;
         }
-
         if(c1.getNumero() > c2.getNumero()) {
             return 1;
         }
-
         return 0;
     }
 }
 
-
+class OrdenaPeloNomeDoTitular implements Comparator<Conta> {
+    @Override
+    public int compare(Conta c1, Conta c2) {
+        String nomeC1 = c1.getTitular().getNome();
+        String nomeC2 = c2.getTitular().getNome();
+        return nomeC1.compareTo(nomeC2);
+    }
+}
