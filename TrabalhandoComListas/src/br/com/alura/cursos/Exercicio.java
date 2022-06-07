e1b6f36f4296de1d2e20e7ae64f25224f73b7169
Regis Pinto <regispinto65@gmail.com>
2022-06-07 12:08:09 -0300

    Java e java.io: Streams, Reader e Writers

---
+++ TrabalhandoComListas/src/br/com/alura/cursos/Exercicio.java
@@ -0,0 +1,17 @@
+package br.com.alura.cursos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Exercicio {
+    public static void main(String[] args) {
+        Map<Integer, String> pessoas = new HashMap<>();
+
+        pessoas.put(21, "Leonardo Cordeiro");
+        pessoas.put(27, "Fabio Pimentel");
+        pessoas.put(19, "Silvio Mattos");
+        pessoas.put(23, "Romulo Henrique");
+
+        System.out.println(pessoas.keySet() + " - " + pessoas.values());
+    }
+}
---
+++ java-io/src/br/com/alura/io/teste/TesteLeitura.java
@@ -0,0 +1,24 @@
+package br.com.alura.io.teste;
+
+import java.io.*;
+
+public class TesteLeitura {
+    public static void main(String[] args) throws IOException {
+        LeArquivoTexto();
+    }
+
+    public static void LeArquivoTexto() throws IOException {
+        InputStream fis = new FileInputStream("lorem.txt");
+        Reader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        String linha = br.readLine();
+
+        while (linha != null) {
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+
+        br.close();
+    }
+}
