7dc399785ca1116a3818d394bd05c1beadc5901e
Regis Pinto <regispinto65@gmail.com>
2022-06-23 15:10:29 -0300

    Java e java.io: Streams, Reader e Writers  - Entendente Serialização em Java

--- java-io/src/br/com/alura/io/teste/TesteSerializacao.java
+++ java-io/src/br/com/alura/io/teste/TesteSerializacao.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.sql.SQLOutput;
 
 public class TesteSerializacao {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         CriaArquivoDeSaida();
 
         CriaArquivoDeEntrada();
